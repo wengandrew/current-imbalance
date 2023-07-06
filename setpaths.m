5c841a5af132bc9cda6256f87bf15c0d0a0f9119
Andrew Weng <weng.andrew@gmail.com>
2023-07-06 08:22:13 -0400

    Initial commit

---
+++ setpaths.m
@@ -0,0 +1,11 @@
+function setpaths()
+% Custom addpath function for this project
+
+addpath 'data/'
+addpath 'figures-2022mecc/'
+addpath 'figures-2023dsmc/'
+addpath 'src/'
+addpath 'src/functions/'
+addpath 'src/utils/'
+
+end
